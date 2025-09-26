4a4792bf0caa301ce8af556bb70f86f228b69df1
Ali Aryani <lenesis@proton.me>
2025-09-26 20:55:32 +0330

    write into data.txt

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
+#include <fstream>
+#include <ios>
 #include <iostream>
 using namespace std;
 
 auto main() -> int {
+    ofstream data("data.txt",ios::out);
+    if(data.is_open()){
+        data << "Here is some text.";
+    } else {
+        cout << "failed to open the file";
+    }
 }
