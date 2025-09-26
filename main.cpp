79f18714a113cb9ad682e1394652e7ee62c1aeca
Ali Aryani <lenesis@proton.me>
2025-09-26 21:04:10 +0330

    read from data.txt

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include <fstream>
-#include <ios>
+// #include <ios>
 #include <iostream>
 using namespace std;
 
 auto main() -> int {
+    /*
     ofstream data("data.txt",ios::out);
     if(data.is_open()){
         data << "Here is some text.";
     } else {
         cout << "failed to open the file";
+    }
+    */
+
+    ifstream file("data.txt");
+    string line{};
+    while(getline(file,line)){
+        cout << line << endl;
     }
 }
